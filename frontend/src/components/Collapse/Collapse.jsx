d45a141120108d591522b0b297bd9d596d582fe4
Moi <lopesloic64@gmail.com>
2023-06-26 06:58:16 +0200

    Création de la page About
    Ajout des logos et images 
    
    Création de la page Home
            Création composant : Collapse.jsx / Collapse.scss
    Intégration configuration des composant et
                  Banner.jsx / (nouveau) Banner_about.scss
                 Collapse.jsx
    dans About.jsx
        
     Mise en page dans About.scss

---
+++ frontend/src/components/Collapse/Collapse.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import icon_arrow from '../../assets/icons/arrow.png'
+function Collapse(props) {
+  const [isOpen, setIsOpen] = useState(false)
+  return (
+    <div className="Collapse">
+      <div className="Collapse__header">
+        {props.title}
+        <button onClick={() => setIsOpen((previousState) => !previousState)}>
+          <img
+            className={`arrow' ${isOpen ? 'arrow-up' : 'arrow-down'}`}
+            src={icon_arrow}
+            alt={isOpen ? 'close button' : 'open button'}
+          />
+        </button>
+      </div>
+      {Array.isArray(props.content) ? (
+        <ul className={isOpen ? 'content' : 'content--hidden'}>
+          {props.content.map((equipment, index) => (
+            <li key={`${equipment}-${index}`}>{equipment}</li>
+          ))}
+        </ul>
+      ) : (
+        <div className={isOpen ? 'content' : 'content--hidden'}>
+          {props.content}
+          {props.children}
+        </div>
+      )}
+    </div>
+  )
+}
+export default Collapse
--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -1,10 +1,48 @@
+import Banner from '../../components/Banner/Banner'
+import lac from '../../assets/images/lac.png'
+import Collapse from '../../components/Collapse/Collapse'
 function About() {
   return (
-    <section className="testabout">
-      <div>Test de la page About</div>
-      <div>blabla</div>
-    </section>
+    <main>
+      <Banner
+        class="banner_about"
+        image={lac}
+        alt="Panoramic Mountain and Forest"
+      />
+      <section className="about">
+        <Collapse title="Fiabilité ">
+          <span>
+            Les annonces postées sur Kasa garantissent une fiabilité totale. Les
+            photos sont conformes aux logements, et toutes les informations sont
+            régulièrement vérifiées par nos équipes.
+          </span>
+        </Collapse>
+        <Collapse title="Respect ">
+          <span>
+            La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+            comportement discriminatoire ou de perturbation du voisinage
+            entraînera une exclusion de notre plateforme.
+          </span>
+        </Collapse>
+        <Collapse title="Service ">
+          <span>
+            Nos équipes se tiennent à votre disposition pour vous fournir une
+            expérience parfaite. N'hésitez pas à nous contacter si vous avez la
+            moindre question.
+          </span>
+        </Collapse>
+        <Collapse title="Sécurité ">
+          <span>
+            La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
+            les voyageurs, chaque logement correspond aux critères sécurité
+            établis par nos services. En laissant une notre aussi bien l'hôte
+            qu'au locataire, cela permet à nos équipes de vérifier que les
+            standards sont bien respectés. Nous organisons également des
+            ateliers sur la sécurité domestique pour nous.
+          </span>
+        </Collapse>
+      </section>
+    </main>
   )
 }
-
 export default About
